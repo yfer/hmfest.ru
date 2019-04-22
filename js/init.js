48e720bc916aa8442ac00fab392d65420e1bcdc9
Mikhail Barabanov <barabanov.mikle@gmail.com>
2019-04-22 18:00:21 +0300

    init version of form

---
+++ js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+    $('.select').formSelect();
+    $('.tabs').tabs();
+    $('.datepicker').datepicker();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
